b5fb1d36c7ff64aba7929418c89aa350324a858c
Colin Aardsma <colin.aardsma@gmail.com>
2016-08-29 20:21:41 -0500

    most of the way there, still needs individual pages and deletion

--- main.py
+++ main.py
@@ -25,12 +25,19 @@ class Blog(db.Model):
     created = db.DateTimeProperty(auto_now_add = True) #sets created to equal current date/time
 
 class MainPage(Handler):
-    def render_front(self, title="", body="", error="", blogs=""):
-        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 10") #table is named Blog because class is named Blog (the class creates the table)
-        self.render("index.html", title=title, body=body, error=error, blogs=blogs)
+    def render_list(self, blogs=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5") #table is named Blog because class is named Blog (the class creates the table)
+        self.render("list.html", blogs=blogs)
 
     def get(self):
-        self.render_front()
+        self.render_list()
+
+class NewPost(Handler):
+    def render_post(self, title="", body="", error=""):
+        self.render("new_post.html", title=title, body=body, error=error)
+
+    def get(self):
+        self.render_post()
 
     def post(self):
         title = self.request.get("title")
@@ -39,32 +46,30 @@ class MainPage(Handler):
         if title and body:
             a = Blog(title = title, body = body) #creates new blog object named a
             a.put() #stores a in database
-            self.redirect("/") #sends you back to main page
+            self.redirect('/') #sends you back to new post page
         else:
             error = "Please enter both title and body!"
-            self.render_front(title, body, error)
+            self.render_post(title, body, error)
 
-class Post(Handler):
-    def render_front(self, title="", body="", error="", blogs=""):
-        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 10") #table is named Blog because class is named Blog (the class creates the table)
-        self.render("post.html", title=title, body=body, error=error, blogs=blogs)
+class Archive(Handler):
+    def render_list(self, blogs=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC") #table is named Blog because class is named Blog (the class creates the table)
+        self.render("list.html", blogs=blogs)
 
     def get(self):
-        self.render_front()
+        self.render_list()
 
-    def post(self):
-        title = self.request.get("title")
-        body = self.request.get("body")
+class DeletePost(Handler):
+    def render_delete(self, blogs=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC") #table is named Blog because class is named Blog (the class creates the table)
+        self.render("delete_post.html", blogs=blogs)
 
-        if title and body:
-            a = Blog(title = title, body = body) #creates new blog object named a
-            a.put() #stores a in database
-            self.redirect("/") #sends you back to main page
-        else:
-            error = "Please enter both title and body!"
-            self.render_front(title, body, error)
+    def get(self):
+        self.render_delete()
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/post' Post)
+    ('/new_post', NewPost),
+    ('/archive', Archive),
+    ('/delete_post', DeletePost)
 ], debug=True)
